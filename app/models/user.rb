2fe90fe70ee9e83b8717c5e4899f3a31d4d02c6e
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2020-03-10 18:24:52 +0000

    add admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    has_many :posts
+    has_many :posts, dependent: :destroy
     before_save {self.email = email.downcase}
     validates :username, presence: true, uniqueness: {case_sensitive: false}, 
     length: {minimum: 3, maximum: 25}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200119155057) do
+ActiveRecord::Schema.define(version: 20200310172049) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20200119155057) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
