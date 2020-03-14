4172504361260da851f8b36603b59847e87a5804
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2020-03-14 17:13:19 +0000

    add tests for category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200310172049) do
+ActiveRecord::Schema.define(version: 20200314165031) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
