f789c39239877b1b34ae8aa72f7f849573a9e060
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2020-03-29 18:46:28 +0100

    add categories to view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
     belongs_to :user
+    has_many :post_categories 
+    has_many :categories, through: :post_categories
     validates :name, :presence => true
     validates :title, :presence => true, :length => { :minimum => 5 }
     validates :user_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200314165031) do
+ActiveRecord::Schema.define(version: 20200316190057) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
   end
 
   create_table "posts", force: :cascade do |t|
